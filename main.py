c40f5a1d9f10cca104facc5cb6356017d869b49d
Jason Mize <jasonemize@gmail.com>
2016-09-09 12:07:36 -0400

    error checking for bad city

--- main.py
+++ main.py
@@ -11,15 +11,27 @@ def main():
 
     payload = {
         "appid": api_key,
-        "q": 'Lexington, US',
+        "q": ';assdf',
         "units": "imperial",
     }
 
     r = requests.get("http://api.openweathermap.org/data/2.5/weather",
                      params=payload)
-    print(r.url)
+ 
+
+
+    # print(r.url)
     weather = r.json()
-    print("Current Temp: {}".format(weather["main"]["temp"]))
+    
+    if  weather["cod"] == "404":
+        print ("That is not a real city.")
+
+
+
+
+
+
+    # print("Current Temp: {}".format(weather["main"]["temp"]))
 
 if __name__ == '__main__':
     main()
